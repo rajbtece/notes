0da3d90f273e56d2288b8c5ae07714722b412f91
Rajkumar <rajbtece@gmail.com>
2023-10-18 20:52:19 +0530

    Update extract-sip.js

--- extract-sip.js
+++ extract-sip.js
@@ -1,21 +1,48 @@
 // S P
 var [head, ...list_of_sips] = document.querySelectorAll("#app > div:nth-child(2) > div.container.body-container.main-body-container > div > div > div.mf-table__container--desktop > div > ul > li");
 
-// .forEach( (n,i) => console.log(n.innerText.replace(/\n/g, "=")))
+const extractRow = async (n, delay = 1000) => {
+    console.log("Fund Name: " + n.querySelector('.fund-name').innerText + "; " + Date.now());
+    if (n.querySelector('.fund-row__container--overview') !== null) {
+        n.querySelector(".fund-name").click();
+    }
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            let name = "";
+            let type = "";
+            let isbn = "";
+            let category = "";
+            let subCategory = "";
+            let frequency = "";
+            let sipAmount = "";
+            try {
+                name = n.querySelector('.fund-name').innerText;
+                let header = n.querySelector('.fund-header .align-items-center').childNodes;
+                type = header[1].innerText;
+                category = header[3].innerText;
+                subCategory = header[5].innerText;
+                isbn = header[0].querySelector('a').href.split('/')[5];
+                frequency = n.querySelectorAll('.main-container__detail--body > div > div')[2].childNodes[1].innerText;
+                sipAmount = n.querySelector('div.main-container__detail--header > div > div.three.columns > div > div').childNodes[1].innerText.replace(/,/g, '');;
+            } catch (err) {
+                console.log(err);
+            }
+            let data = [name, type, category, subCategory, frequency, sipAmount, isbn];
+            resolve({ 'data': data.join("="), 'transaction': [] });
+        }, delay);
+    });
+}
 
+const process = async (sips = []) => {
+    let results = [];
+    for (let i = 0; i < sips.length; i++) {
+        const result = await extractRow(sips[i]);
+        results.push(result.data);
+    }
+    let _data = "\n\nFund=Type=Category=Sub Category=Frequency=Amount=ISIN\n" + results.join("\n");
+    console.log(_data);
+}
 
-var _data = "\n\nFund=Type=Category=Sub Category=Frequency=Amount=Status\n" + list_of_sips.map( (n,i) => {
-const name = n.querySelector('.fund-name').innerText;
-const meta = n.querySelector('.fund-metasubtext__container');
-const type = meta.childNodes[0].textContent;
-const category = meta.childNodes[2].textContent;
-const subCategory = meta.childNodes[4].textContent;
+process(list_of_sips);
 
-const details = n.querySelector('.main-container__overview');
-const frequency = details.childNodes[3].textContent;
-const sipAmount = details.childNodes[4].querySelector('.text-16').innerText.replace(",", "");
-const status = details.childNodes[1].textContent;
-const data = [name, type, category, subCategory, frequency, sipAmount, status];
-return data.join("=");
-}).join("\n");
-console.log(_data + "\n\n");
+//process([list_of_sips[0], list_of_sips[1]]);
